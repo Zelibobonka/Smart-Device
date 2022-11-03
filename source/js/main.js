13b9ef31d1d052bd0fb7579f1744551b2f45e53e
Алексей Михайлов <alexeysampds@gmail.com>
2022-11-03 11:45:51 +0400

    Встаем на путь исправления 2

--- source/js/main.js
+++ source/js/main.js
@@ -2,7 +2,6 @@ import './vendor.js';
 import {iosVhFix} from './utils/ios-vh-fix';
 import {initModals} from './modules/modals/init-modals';
 import {showTextHidden} from './modules/open-text-hidden';
-import {scroll} from './modules/scroll';
 import {realizationAccordion} from './modules/accordion';
 import {addMask} from './modules/add-mask';
 
@@ -15,7 +14,6 @@ window.addEventListener('DOMContentLoaded', () => {
   // Modules
 
   showTextHidden();
-  scroll();
   realizationAccordion();
   addMask();
 
