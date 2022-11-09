2333a701520b8905411f2ba7eb45beab2f4947e0
Алексей Михайлов <alexeysampds@gmail.com>
2022-11-09 11:13:59 +0400

    Исправления после тестирования

--- source/js/modules/add-mask.js
+++ source/js/modules/add-mask.js
@@ -4,6 +4,15 @@ const tel = document.querySelectorAll('[type="tel"]');
 
 const addMask = () => {
   tel.forEach((element) => {
+
+    element.oninput = function(event) {
+      event.target.setCustomValidity('');
+    };
+
+    element.oninvalid = function(event) {
+      event.target.setCustomValidity('Количество цифр номера телефона должно быть 10');
+    };
+
     const maskOptions = {
       mask: '+{7}(000)000-00-00'
     };
